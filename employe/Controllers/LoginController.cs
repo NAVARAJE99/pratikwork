cfd14b0f31ecf1b7abffc393308c842f801a3666
v - Navanath Pawar <navanath.pawar@avalara.com>
2023-06-28 10:23:58 +0530

    'initial'

---
+++ employe/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using employe.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace employe.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel loginViewModel)
+        {
+            LoginViewModel loginViewModel1=new LoginViewModel();
+            return View();
+        }
+    }
+}
---
+++ employe/ViewModel/All.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace employe.ViewModel
+{
+    public class All
+    {
+    }
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
